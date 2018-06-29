64ab7da4b8c1a25daa8114ca4d77b3e65fee4417
Andrey F. Mindubaev <covex.mobile@gmail.com>
2018-06-29 23:43:44 +0300

    Fix travis build

--- .travis/before_script_test.sh
+++ .travis/before_script_test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 set -ev
 
-TESTING_SCRIPTS_DIR=vendor/symfony-cmf/testing/bin
-CONSOLE=${TESTING_SCRIPTS_DIR}/console
-
-@echo '+++ create PHPCR +++'
-${CONSOLE} doctrine:phpcr:init:dbal --drop --force
-${CONSOLE} doctrine:phpcr:repository:init
+vendor/symfony-cmf/testing/bin/console doctrine:phpcr:init:dbal --drop --force
+vendor/symfony-cmf/testing/bin/console doctrine:phpcr:repository:init
