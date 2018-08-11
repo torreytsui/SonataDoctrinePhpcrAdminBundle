ad765a066fb22ad2964b40bc5cafc635f7ba8c14
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-08-11 07:28:43 +0200

    remove older merge relict

--- .travis/before_script_test.sh
+++ .travis/before_script_test.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -ev
 
-vendor/symfony-cmf/testing/bin/console doctrine:phpcr:init:dbal --drop --force
-vendor/symfony-cmf/testing/bin/console doctrine:phpcr:repository:init
-
 TESTING_SCRIPTS_DIR=vendor/symfony-cmf/testing/bin
 CONSOLE=${TESTING_SCRIPTS_DIR}/console
 
